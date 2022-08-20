84b2ec89933e3550a57b40913b72efaf003d4349
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-08-20 11:29:52 -0500

    Update WordtoPDF.py

--- WordtoPDF.py
+++ WordtoPDF.py
@@ -3,8 +3,8 @@ import win32com.client
 
 wdFormatPDF = 17
 
-inputFile = os.path.abspath(r"C:\Users\r6311\Downloads\College and Career\Resume\Richie Zhou's Resume 2022.docx")
-outputFile = os.path.abspath(r"C:\Users\r6311\Downloads\College and Career\Resume\Richie Zhou's Resume 2022.pdf")
+inputFile = os.path.abspath(r"") #Inputfile absolute location path
+outputFile = os.path.abspath(r"") #Outputfile absolute location path
 word = win32com.client.Dispatch('Word.Application')
 doc = word.Documents.Open(inputFile)
 doc.SaveAs(outputFile, FileFormat=wdFormatPDF)
