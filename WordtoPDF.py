a069fb069db47e65eab6bc40627cdf56d58746fa
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-08-31 10:49:37 -0500

    Update WordtoPDF.py

--- WordtoPDF.py
+++ WordtoPDF.py
@@ -1,20 +1,23 @@
 ## Libraries
 import win32com.client
 
-wdFormatPDF = 17
+def WordtoPDF():
+    wdFormatPDF = 17
 
-## User inputs
-print("Enter a input file location: ", end = "")
-inputFile = input() + ".docx"
+    ## User inputs
+    print("Enter a input file location: ", end = "")
+    inputFile = input() + ".docx"
 
-print("Enter a output file location: ", end = "")
-outputFile = input() + ".pdf"
+    print("Enter a output file location: ", end = "")
+    outputFile = input() + ".pdf"
 
-## Convert Word to PDF
-word = win32com.client.Dispatch('Word.Application')
-doc = word.Documents.Open(inputFile)
-doc.SaveAs(outputFile, FileFormat=wdFormatPDF)
-doc.Close()
-word.Quit()
+    ## Convert Word to PDF
+    word = win32com.client.Dispatch('Word.Application')
+    doc = word.Documents.Open(inputFile)
+    doc.SaveAs(outputFile, FileFormat=wdFormatPDF)
+    doc.Close()
+    word.Quit()
 
-print("Conversion completed!")
+    print("Conversion completed!")
+
+WordtoPDF()
